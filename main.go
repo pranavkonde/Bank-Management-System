be58d0608461fce94ee9be5c84656c527e746850
Pranav Konde <pranavkonde2020@gmail.com>
2022-07-29 15:43:49 +0530

    Added main file

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+import "fmt"
+type customer struct{``
+	email_id string
+	phone_no string
+	account_ID int
+	password string
+	balance int
+	trans_amount int 
+	trans_type string
+}
+func Accountant(){
+	
+}
+func main(){
+	fmt.Println("Welcome")
+	fmt.Println("To whom you have to connect with 1. Accountant 2. Customer")
+	var a string
+	a:= fmt.Scanln(&a)
+	if ToLower(a)=="Accountant"{
+		Accountant()
+	}
+	fmt.Println("Enter the email-id:")
+	fmt.Println("Enter the email-id:")
+	cust = customer{
+		email_id:=,
+
+	}
+}
